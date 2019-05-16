4df5e113ac4b4caa0d4ab5843fb813c9686f59d3
Sid <samanduyev@gmail.com>
2019-05-16 12:55:54 -0400

    added association and seed data

--- server/seed.js
+++ server/seed.js
@@ -5,9 +5,12 @@ const createModels = async()=>{
     Users.destroy({where:{}});
     Cuisines.destroy({where:{}});
     const initialUsers = await Users.bulkCreate(users,{returning:true});
-    const initialCuisines = await Cuisines.bulkCreate(cuisines);
+    const initialCuisines = await Cuisines.bulkCreate(cuisines,{returning:true});
     const initialReceipies = await Receipies.bulkCreate(receipies,{returning:true});
-
+    for(i=0;i<initialReceipies.length;i++){
+      await initialReceipies[i].setUser(initialUsers[0]);
+      await initialReceipies[i].addCuisines(initialCuisines[2])
+    }
   }catch(e){
     console.log(e.message);
   }finally{
